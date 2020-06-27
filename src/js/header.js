0442fa1ebf1fb19eeebf84ba583096ca1bb54e42
Константин Остроухов <kostyaostr@jetstyle.ru>
2020-06-28 00:33:14 +0500

    before shader

---
+++ src/js/header.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import Button from './button.js'
+const menuContent = [
+    {
+        name: 'Концепция',
+        link: '', 
+    },
+    {
+        name: 'Основной знак',
+        link: '', 
+    },
+    {
+        name: 'Герб-контейнер',
+        link: '', 
+    },
+    {
+        name: 'Краткая форма',
+        link: '', 
+    },
+    {
+        name: 'Цвета',
+        link: '', 
+    },
+    {
+        name: 'Гарнитура',
+        link: '', 
+    },
+    {
+        name: 'Паттерн',
+        link: '', 
+    },
+    {
+        name: 'Носители',
+        link: '', 
+    },
+]
+
+
+
+class Header extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div className = 'header'>
+                <div className = 'content'>
+                    <div className = 'menuNavigation'>
+                        {
+                            menuContent.map(
+                                (item, index)=>{
+                                    return(
+                                        <a className = 'menuUnit' href = {item.link}>
+                                            {item.name}
+                                        </a>
+                                    )
+                                }
+                            )
+                        }
+                    </div>
+                    <Button text = 'Скачать как PDF'/>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default Header
---
+++ src/js/lottieAnimation.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import lottie from 'lottie-web'
+
+class LottieAnimation extends React.Component{
+    constructor(props){
+        super(props)
+        this.ref = React.createRef()
+    }
+    componentDidMount(){
+        lottie.loadAnimation({
+            container: this.ref.current, // the dom element that will contain the animation
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            path: this.props.path // the path to the animation json
+        });
+    }
+    render(){
+        return(
+            <div ref = {this.ref} className = 'LottieAnimation'>
+
+            </div>
+        )
+    }
+}
+export default LottieAnimation
